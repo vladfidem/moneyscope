68c3610a9c78a0d8cc145e187619161cfdb1159a
vladfidem <vladfidem@gmail.com>
2025-06-04 18:25:20 +0300

    feat: project init and dependencies setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 
-export default function App() {
+export const App: FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar style="auto"/>
+      <Text>MoneyScope App</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
